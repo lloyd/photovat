152f0041a663a863a5226adeae268ece59284d66
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-03-05 18:43:51 -0700

    initial implementation of link dir creation

--- createlinkdir.py
+++ createlinkdir.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python
 
-import 'os'
+import os, hashlib
+
+def calc_md5(path):
+    try:
+        fp = open(path, 'rb')
+        try:
+            sig = hashlib.md5()
+            while True:
+                chunk = fp.read(1024 * 16)
+                if not chunk: break
+                sig.update(chunk)
+            return sig.hexdigest()
+        finally:
+            fp.close()
+    except IOError:
+        return 'error'
+
+linkDir = "linkdir"
+os.mkdir(linkDir)
 
 for root, dirs, files in os.walk('.'):
     for name in files:
-        print os.join(root, name))
+        if name.endswith(".jpg") or name.endswith(".JPG"):
+            p = os.path.abspath(os.path.join(root, name))
+            if not os.path.islink(p): 
+                md5 = calc_md5(p)
+                tgt = os.path.join(linkDir, md5 + ".jpg")
+                if not os.path.islink(tgt):
+                    os.symlink(p, tgt)
+                
